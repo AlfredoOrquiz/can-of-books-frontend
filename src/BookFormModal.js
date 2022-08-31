0301a834915222df20ca8ab4e6efc84542c1881e
Alfredo <fredy_rios0123@outlook.com>
2022-08-30 22:23:59 -0700

    Lab 12 is not finished. Needs more work.

--- src/BookFormModal.js
+++ src/BookFormModal.js
@@ -7,19 +7,22 @@ class BookFormModal extends React.Component {
       <Modal show = {this.props.show} onHide = {this.props.onHide}>
         <Form onSubmit={this.props.handleAddBook}>
           <Form.Group className='mb-3' controlId='title'>
-            <Form.Label >title</Form.Label>
+            <Form.Label >Title</Form.Label>
             <Form.Control type='text' name='title' placeholder='Enter a book name' />
           </Form.Group>
           <Form.Group className='mb-3' controlId='description'>
-            <Form.Label>description</Form.Label>
+            <Form.Label>Description</Form.Label>
             <Form.Control type='text' name='description' placeholder='Enter the description' />
           </Form.Group>
 
           <Form.Group className='mb-3' controlId='checkBox'>
-            <Form.Check type='checkbox' name='checkBox' label='Have you read the book?' />
+            <Form.Check type='checkbox' name='checkBox' label='Read' />
           </Form.Group>
           <Button onClick={this.props.onHide} variant='primary' type='submit'>
             Submit
+          </Button>
+          <Button onClick={this.props.onHide} variant='primary' type='submit'>
+            Delete
           </Button>
         </Form>
       </Modal>
